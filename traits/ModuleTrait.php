9802fb2cf206d21ce2b76702dab0b1bb9fd313fa
ronashdkl <ronashdkl@gmail.com>
2018-05-11 13:41:08 +0545

    v1

---
+++ traits/ModuleTrait.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace ronash\user\traits;
+
+use ronash\user\Module;
+
+/**
+ * Trait ModuleTrait
+ *
+ * @property-read Module $module
+ * @package dektrium\user\traits
+ */
+trait ModuleTrait
+{
+    /**
+     * @return Module
+     */
+    public function getModule()
+    {
+        return \Yii::$app->getModule('user');
+    }
+
+    /**
+     * @return string
+     */
+    public static function getDb()
+    {
+        return \Yii::$app->getModule('user')->getDb();
+    }
+}
